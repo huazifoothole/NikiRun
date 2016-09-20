e740442bf5860150a64586bd938e696c703b72aa
huazifoothole <shiyuedehupo@163.com>
2016-09-21 00:09:09 +0800

    git test1

--- src/com/example/nikirun/MainActivity.java
+++ src/com/example/nikirun/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends Activity implements NavigationDrawerFragment.N
 		
 		SDKInitializer.initialize(getApplicationContext());
 		
-		  
+		  //git test1
 		setContentView(R.layout.activity_main);
 		 
 		//This does the magic! 隐藏actionbar左侧图标  transparent透明的
